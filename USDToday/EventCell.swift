a8bffb08d35b0e4908b7520a42e7cbdccec4d5d8
Maeve McClatchey <maevemcclatchey@sandiego.edu>
2018-04-30 14:24:02 -0700

    Created views and segues

---
+++ USDToday/EventCell.swift
@@ -0,0 +1,20 @@
+//
+//  EventCell.swift
+//  USDToday
+//
+//  Created by Maeve McClatchey on 4/30/18.
+//  Copyright © 2018 University of San Diego. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class EventCell: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var detail: UILabel!
+    
+}
+
+
